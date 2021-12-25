d165b873f38e879d1a7e8cb1fbcbfb52f0496b64
Przemqo <przemyslaw.baran09@gmail.com>
2021-12-25 13:43:08 +0100

    Added login page

--- src/main/java/com/example/application/views/list/DashboardView.java
+++ src/main/java/com/example/application/views/list/DashboardView.java
@@ -11,8 +11,11 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
+import javax.annotation.security.PermitAll;
+
 @Route(value = "dashboard", layout = MainLayout.class)
 @PageTitle("Dashboard | Vaadin CRM")
+@PermitAll
 public class DashboardView extends VerticalLayout {
 
     private CrmService service;
